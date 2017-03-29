b2b66e02f7e53a00accfec08fcd28625937d114f
nolan32 <jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.fr>
2017-03-29 14:08:12 +0200

    Ajout de l'exo 1/2/3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-(function(){
+//exo 1
+$("#removeDiv").click(function(){
+	$("div.remove").remove();
+	$(this).parent().remove();
+	});
 
-    // Todo
+//exo2
+$( "#setText" ).keyup(function(){
+    var caractere = $( this ).val();
+    $( "#needText" ).text( caractere )
 
-})();
+});
 
 
+//exo3
+
+$("#setColor").click(function(){
+	$("#changeColor").css('background-color','red');
+});
